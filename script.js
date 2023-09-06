0cea365481109bf4fde4566006a709cd4426b065
bargog29 <baranga.georgemarius@gmail.com>
2023-09-06 20:44:40 +0300

    New Sections and Features

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 'use strict'
 
+//Typing animation in hero section
+const typed = new Typed('.multiple--text', {
+    strings: ['Frontend Developer', 'Web Designer', 'UI/UX Designer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
